7c61ffe918f66d3c66b4b58b247ddb040ae790d5
Carl Meyer <carl@oddbird.net>
2013-02-28 12:08:48 -0700

    Add bin/watch and point Compass to source dirs, not hovercraft output dir.

--- config.rb
+++ config.rb
@@ -10,10 +10,10 @@ project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "sass"
-css_dir = "output/css"
-images_dir = "output/images"
-fonts_dir = "output/fonts"
-javascripts_dir = "output/js"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
 line_comments = false
 preferred_syntax = :scss
 output_style = :expanded
